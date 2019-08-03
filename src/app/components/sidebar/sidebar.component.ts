d08093cb2877ee4a2b718e972385f455bf1550f7
Danilo Martinez <danilo_98osorno@hotmail.com>
2019-08-03 16:55:58 -0500

    Se agregan modulos de componentes que permiten ser utilizados dentro de toda la app

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'app/login/services/user.service';
+import { Router } from '@angular/router';
+
+declare const $: any;
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
+export const ROUTES: RouteInfo[] = [
+  { path: '/overview', title: 'Resumen', icon: 'list', class: '' },
+  { path: '/clients', title: 'Clientes', icon: 'shopping_basket', class: '' },
+  { path: '/dealers', title: 'Consecionarios', icon: 'store', class: '' },
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+    if ($(window).width() > 991) {
+      return false;
+    }
+    return true;
+  };
+
+  logOut() {
+    this.userService.deleteUserLoggedIn();
+    this.router.navigateByUrl('/login');
+}
+}
