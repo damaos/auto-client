a4f511d6a23030c1764c026e2d8611f7a1a7dc41
Danilo Martinez <danilo_98osorno@hotmail.com>
2019-08-04 15:38:57 -0500

    Se insertan los mocks para ciudades, consecionarios y clientes si no hay registros en la bd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { CITIES } from './mocks/cities.mock';
+import { CLIENTS } from './mocks/clients.mock';
+import { DEALERS } from './mocks/dealers.mock';
 
 
 @Component({
@@ -6,6 +9,20 @@ import { Component} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  cities = CITIES;
+  clients = CLIENTS;
+  dealers = DEALERS;
 
+  ngOnInit() {
+    // set cities, clients, dealers in storage
+    const cliExist = localStorage.getItem('clients');
+
+    localStorage.setItem('dealers', JSON.stringify(this.dealers));
+    localStorage.setItem('cities', JSON.stringify(this.cities));
+
+    if (cliExist == null || cliExist === undefined) {
+      localStorage.setItem('clients', JSON.stringify(this.clients));
+    }
+  }
 }
