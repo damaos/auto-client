73e29eed3d2ec12c74b5ee84897531b1993aca23
Danilo Martinez <danilo_98osorno@hotmail.com>
2019-08-04 15:40:15 -0500

    Se agrega los pipes utilizados para filtrar la data de los clientes

--- src/app/clients/clients.module.ts
+++ src/app/clients/clients.module.ts
@@ -6,9 +6,12 @@ import { OverviewModule } from 'app/overview/overview.module';
 import { ClientsService } from './services/clients.service';
 import { ClientUXComponent } from './components/client-ux/client-ux.component';
 import { FormsModule } from '@angular/forms';
+import { SearchPipe } from './pipes/client.pipe';
+import { CityPipe } from './pipes/city.pipe';
+import { DealerPipe } from './pipes/dealer.pipe';
 
 @NgModule({
-  declarations: [ClientsComponent, ClientUXComponent],
+  declarations: [ClientsComponent, ClientUXComponent, SearchPipe, CityPipe, DealerPipe],
   imports: [
     CommonModule,
     AngularMaterialModule,
